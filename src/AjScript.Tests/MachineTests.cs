e097f803f59a40b7d599531fd2ed53e6160ffb18
ajlopez <webmaster@ajlopez.com>
2014-09-11 10:07:46 -0300

    Machine first test

---
+++ src/AjScript.Tests/MachineTests.cs
@@ -0,0 +1,23 @@
+﻿namespace AjScript.Tests
+{
+    using System;
+    using AjScript.Primitives;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MachineTests
+    {
+        [TestMethod]
+        public void CreateMachineWithContext()
+        {
+            Machine machine = new Machine();
+
+            Assert.IsNotNull(machine.Context);
+
+            var result = machine.Context.GetValue("Object");
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ObjectFunction));
+        }
+    }
+}
---
+++ src/AjScript/Machine.cs
@@ -0,0 +1,21 @@
+﻿namespace AjScript
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjScript.Primitives;
+
+    public class Machine
+    {
+        private IContext context;
+
+        public Machine()
+        {
+            this.context = new Context();
+            this.context.SetValue("Object", new ObjectFunction(this.context));
+        }
+
+        public IContext Context { get { return this.context; } }
+    }
+}
