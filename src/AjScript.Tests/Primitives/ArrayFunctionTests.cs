335f35aaef02f6fe3abd7de56549a2cf91eec43a
ajlopez <webmaster@ajlopez.com>
2014-09-12 19:03:17 -0300

    ArrayFunction first test

---
+++ src/AjScript.Tests/Primitives/ArrayFunctionTests.cs
@@ -0,0 +1,24 @@
+﻿namespace AjScript.Tests.Primitives
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjScript.Language;
+    using AjScript.Primitives;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ArrayFunctionTests
+    {
+        [TestMethod]
+        public void NewInstance()
+        {
+            ArrayFunction function = new ArrayFunction(null);
+            object instance = function.NewInstance(null);
+
+            Assert.IsNotNull(instance);
+            Assert.IsInstanceOfType(instance, typeof(ArrayObject));
+        }
+    }
+}
---
+++ src/AjScript/Primitives/ArrayFunction.cs
@@ -0,0 +1,21 @@
+﻿namespace AjScript.Primitives
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjScript.Language;
+
+    public class ArrayFunction : Function
+    {
+        public ArrayFunction(IContext context)
+            : base(null, null, context)
+        {
+        }
+
+        public override object NewInstance(object[] parameters)
+        {
+            return new ArrayObject(new List<object>());
+        }
+    }
+}
