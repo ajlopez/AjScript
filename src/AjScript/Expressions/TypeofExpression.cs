2ece0023b3ce6e4b210039d3e4be6a5722190f83
ajlopez <webmaster@ajlopez.com>
2014-09-22 18:04:33 -0300

    TypeofExpression, WIP

---
+++ src/AjScript/Expressions/TypeofExpression.cs
@@ -0,0 +1,25 @@
+﻿namespace AjScript.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjScript.Language;
+
+    public class TypeofExpression : IExpression
+    {
+        private IExpression expression;
+
+        public TypeofExpression(IExpression expression)
+        {
+            this.expression = expression;
+        }
+
+        public IExpression Expression { get { return this.expression; } }
+
+        public object Evaluate(IContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
