94a5f79410f557df63179c8bf42111065e58284a
ajlopez <webmaster@ajlopez.com>
2015-01-14 07:12:12 -0300

    String function first test

---
+++ src/AjScript.Tests/Primitives/StringFunctionTests.cs
@@ -0,0 +1,25 @@
+﻿namespace AjScript.Tests.Primitives
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjScript.Language;
+    using AjScript.Primitives;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class StringFunctionTests
+    {
+        [TestMethod]
+        public void HasPrototype()
+        {
+            StringFunction function = new StringFunction(null);
+
+            var result = function.GetValue("prototype");
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(StringObject));
+        }
+    }
+}
---
+++ src/AjScript/Primitives/StringFunction.cs
@@ -0,0 +1,24 @@
+﻿namespace AjScript.Primitives
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjScript.Language;
+
+    public class StringFunction : Function
+    {
+        public StringFunction(IContext context)
+            : base(null, null, context)
+        {
+            var prototype = new StringObject();
+
+            this.SetValue("prototype", prototype);
+        }
+
+        public override object NewInstance(object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
